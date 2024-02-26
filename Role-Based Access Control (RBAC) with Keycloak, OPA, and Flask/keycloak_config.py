645b3b4b1adc05de91d76fd679461cc3970eb72d
Rustyryan <44802832+Rustyryan-11@users.noreply.github.com>
2024-02-26 14:30:59 -0600

    Update keycloak_config.py

--- Role-Based Access Control (RBAC) with Keycloak, OPA, and Flask/keycloak_config.py
+++ Role-Based Access Control (RBAC) with Keycloak, OPA, and Flask/keycloak_config.py
@@ -5,3 +5,4 @@ keycloak_openid = KeycloakOpenID(server_url="http://localhost:8080/auth/",
                                  client_id="my_client_app",
                                  realm_name="my_realm",
                                  client_secret_key="my_secret_key")
+
